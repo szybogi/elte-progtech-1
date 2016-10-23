dc0ea9ca8c512eba1dbc3a0275650c4072f59da3
szybogi <szy.bogi@gmail.com>
2016-10-23 16:17:39 +0200

    házi

--- homework4/src/student/Read.java
+++ homework4/src/student/Read.java
@@ -20,6 +20,7 @@ public class Read {
                 result.get(result.indexOf(student)).addGrade(g);
             } else {
                 result.add(student);
+                result.get(result.indexOf(student)).addGrade(g);
             }
         }
         return result;
